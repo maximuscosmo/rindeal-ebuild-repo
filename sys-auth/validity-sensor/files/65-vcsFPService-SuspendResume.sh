a18c6b7bb2f2bbb245a4f27c6ee75fb6da6e9cda
Jan Chren (rindeal) <dev.rindeal@gmail.com>
2019-01-28 12:03:02 +0100

    sys-auth/validity-sensor: use `systemctl kill` instead of pkill
    pkill used to target the shell script itself

--- sys-auth/validity-sensor/files/65-vcsFPService-SuspendResume.sh
+++ sys-auth/validity-sensor/files/65-vcsFPService-SuspendResume.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# Copyright 2016 Jan Chren (rindeal)
+# Copyright 2016, 2019 Jan Chren (rindeal)
 # Distributed under the terms of the GNU General Public License v2
 
 # This script was ported from:
@@ -9,13 +9,13 @@
 resume_FPS() {
     # Signal the vcsFPService about the resume
     echo "Sending Resume Event"
-    pkill -SIGUSR2 vcsFPService
+    systemctl kill --signal=SIGUSR2 vcsFPService.service
 }
 
 suspend_FPS() {
     # Signal the vcsFPService about the suspend/hiberante
     echo "Sending Suspend Event"
-    pkill -SIGUSR1 vcsFPService
+    systemctl kill --signal=SIGUSR1 vcsFPService.service
 }
 
 case "$1" in

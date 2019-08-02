037bd72f35621d8a5ae8e07578c90760fa96f0f3
Jan Chren (rindeal) <dev.rindeal@gmail.com>
2019-08-02 18:28:32 +0200

    eclass/tests/arrays.sh: enhance

--- eclass/tests/arrays.sh
+++ eclass/tests/arrays.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-EAPI=6
+EAPI=7
 
 . ./tests-common.sh
 
@@ -8,6 +8,13 @@ DEPEND_A=( {1,2,3} )
 RDEPEND_A=( r{1,2,3} )
 PDEPEND_A=( p{1,2,3} )
 CDEPEND_A=( c{1,2,3} )
+
+inherit arrays
+
+DEPEND_A=( "${DEPEND}" {1,2,3} )
+RDEPEND_A=( "${RDEPEND}" r{1,2,3} )
+PDEPEND_A=( "${PDEPEND}" p{1,2,3} )
+CDEPEND_A=( "${CDEPEND}" c{1,2,3} )
 
 inherit arrays
 
